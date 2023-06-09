bf714d38e602ea63691184897722650b27625e84
pxwjj <wangpx@koal.com>
2023-06-09 11:07:20 +0800

    "modif README.md add vim config"

--- c_process_test/hello.c
+++ c_process_test/hello.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main()
+int math(int a , int b)
 {
+    return a+b;
+}
+
+int main()
+{  
+    int c;
+    c = math(1,1);
     printf("hello world\n");
+    printf("1 + 1 = %d\n" , c);
     return 0;
 }
